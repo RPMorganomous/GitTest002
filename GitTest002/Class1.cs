3695ec04ddd52b095b55f4b76bcbcaa37f8dc4a3
Rick Morgan <rmorgan@claimpilot.com>
2017-10-27 16:50:32 -0500

    4th line is gone and now there is a 5th line but we are on the differentBranch

--- GitTest002/Class1.cs
+++ GitTest002/Class1.cs
@@ -5,5 +5,7 @@
         //Here is a test class
         //And a new line has been added
         //Now on origin/master brach
+        //Really did not like that 4th line
+        //Now there is a 5th line and we are on differentBranch
     }
 }
