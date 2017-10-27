b3e4b2ba82eb2a9ff8899dd09db79e6750f18c41
Rick Morgan <rmorgan@claimpilot.com>
2017-10-27 16:56:59 -0500

    added a fourth line and changed some stuff

--- GitTest002/Class1.cs
+++ GitTest002/Class1.cs
@@ -5,6 +5,7 @@
         //Here is a test class
         //And a new line has been added
         //Now on origin/master brach
+        //this has changed alot since then
         //Really did not like that 4th line
         //Now there is a 5th line and we are on differentBranch
         //and now a 6th line
