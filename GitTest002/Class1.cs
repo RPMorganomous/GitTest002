79eba70f1b3e9a007fa44bf7d7d0a518e71f799f
Rick Morgan <rmorgan@claimpilot.com>
2017-10-30 13:25:11 -0500

    Revert: added a sixth line

--- GitTest002/Class1.cs
+++ GitTest002/Class1.cs
@@ -1,18 +1,17 @@
-﻿﻿﻿namespace GitTest002
+﻿namespace GitTest002
 {
     public class Class1
     {
         //Here is a test class
         //And a new line has been added
         //Now on origin/master brach
-        //this has changed alot since then
         //Really did not like that 4th line
         //Now there is a 5th line and we are on differentBranch
         //and now a 6th line
         //I really like this, but now I want to experiment...  So let's start a new branch.
-    }  //So now time to abandon this crazy experiment.  For now...  But I had to manually copy this line into my merge. :(
-    
-    //Now on origin/master brach
-    //Really did not like that 4th line
-    //Now there is a 5th line and we are on differentBranch
+        //So what if I make this test line and call it Frankenstein.
+        //Which also rymes, btw...
+        //And now a commit.
+        //with a push.
+    }
 }
