183e882a793df78738896f0bce1b3f6a4b0bf3c4
Rick Morgan <rmorgan@claimpilot.com>
2017-10-30 13:17:15 -0500

    Missing bunch of lines

--- GitTest002/Class1.cs
+++ GitTest002/Class1.cs
@@ -11,4 +11,8 @@
         //and now a 6th line
         //I really like this, but now I want to experiment...  So let's start a new branch.
     }  //So now time to abandon this crazy experiment.  For now...  But I had to manually copy this line into my merge. :(
+    
+    //Now on origin/master brach
+    //Really did not like that 4th line
+    //Now there is a 5th line and we are on differentBranch
 }
